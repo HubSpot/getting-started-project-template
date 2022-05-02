608c858190aa9b786cd3a07c904b1bb5088f6d94
Ben Anderson <banderson@hubspot.com>
2022-05-02 12:06:40 -0400

    feat: Migrate card to Custom Tab

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -1,31 +1,33 @@
 const axios = require("axios");
 
 exports.main = async (context = {}, sendResponse) => {
-  const demoObject = {
-    objectId: 1,
-    title: "Sample project custom CRM card",
-    desc: `A custom CRM card is a UI extension that displays custom info from either HubSpot or an external system. It can also include custom actions—click the "Get Inspired" button to see example data from the ZenQuotes API.`,
-  };
-
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
 
     sendResponse({
-      results: [
-        demoObject,
+      sections: [
         {
-          objectId: 2,
-          link: "https://zenquotes.io/",
-          title: "Inspirational quotes provided by ZenQuotes API",
-          quote: data[0].q,
-          author: data[0].a,
+          type: 'descriptionList',
+          items: [
+            { label: 'Quote', value: data[0].q },
+            { label: 'Author', value: data[0].a }
+          ]
         },
-      ],
-      primaryAction: {
-        type: "SERVERLESS_ACTION_HOOK",
-        serverlessFunction: "crm-card",
-        label: "Get Inspired",
-      },
+        {
+          type: 'button',
+          text: 'Get Inspired',
+          onClick: {
+            type: "SERVERLESS_ACTION_HOOK",
+            serverlessFunction: "crm-card",
+          },
+        },
+        {
+          type: 'text',
+          format: 'markdown',
+          variant: 'microcopy',
+          text: "_Quotes provided by [ZenQuotes API](https://zenquotes.io/)_",
+        },
+      ]
     });
   } catch (error) {
     throw new Error(`There was an error fetching the quote': ${error.message}`);
