e1c72845a7537abb7ac16f4b0c65ffb321820c1b
Branden Rodgers <brodgers@hubspot.com>
2022-08-05 12:06:03 -0400

    update error handling. Add simple readme

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -1,5 +1,3 @@
-
-
 // For external API calls
 const axios = require('axios');
 
@@ -14,29 +12,27 @@ exports.main = async (context = {}, sendResponse) => {
     text: "_An example of a CRM card extension that displays data from Hubspot, uses ZenQuotes public API to display daily quote, and demonstrates custom actions using serverless functions._",
   };
 
-  let quoteSections = [];
-
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
 
-    quoteSections = [
+    const quoteSections = [
       {
         type: "tile",
         body: [
           {
             type: "text",
             format: "markdown",
-            text: "**Hello " + firstname + ", here's your quote for the day**!"
+            text: `**Hello ${firstname}, here's your quote for the day**!`
           },
           {
             type: "text",
             format: "markdown",
-            text: "**Quote**: " + data[0].q
+            text: `**Quote**: ${data[0].q}`
           },
           {
             type: "text",
             format: "markdown",
-            text: "**Author**: " + data[0].a
+            text: `**Author**: ${data[0].a}`
           }
         ]
       },
@@ -49,20 +45,16 @@ exports.main = async (context = {}, sendResponse) => {
         }
       }
     ];
-  } catch (error) {
-    quoteSections = [
-      {
-        type: "alert",
-        variant: "danger",
-        title: "",
-        body: {
-          type: "text",
-          text: "Error fetching new quote"
-        }
-      }
-    ];
-    console.log(error);
-  }
 
-  sendResponse({ sections: [introMessage, ...quoteSections] });
+    sendResponse({ sections: [introMessage, ...quoteSections] });
+  } catch (error) {
+    // Creates an error feedback banner when it catches an error
+    sendResponse({
+      message: {
+        type: 'ERROR',
+        body: `Error: ${error.message}`
+      },
+      sections: [introMessage]
+    });
+  }
 };
